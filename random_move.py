fe22b4b2e37999c66166456d62765017e1599aee
Conner Jensen <connerj70@gmail.com>
2020-04-15 19:20:00 -0600

    Fix generate_random_move to take in a tile

--- random_move.py
+++ random_move.py
@@ -1,15 +1,15 @@
-from random import randint
+from random import choice
+from tile import Tile
 
 
-def generate_random_move():
-    randomInt = randint(1, 4)
-    if randomInt == 1:
-        return "up"
-    elif randomInt == 2:
-        return "right"
-    elif randomInt == 3:
-        return "down"
-    elif randomInt == 4:
-        return "left"
-    # Just in case let's always return a direction
-    return "right"
+def generate_random_move(current_tile):
+    possible_move_directions = []
+    if current_tile.up != None and current_tile.up.passable == True:
+        possible_move_directions.append("up")
+    if current_tile.right != None and current_tile.right.passable == True:
+        possible_move_directions.append("right")
+    if current_tile.down != None and current_tile.down.passable == True:
+        possible_move_directions.append("down")
+    if current_tile.left != None and current_tile.left.passable == True:
+        possible_move_directions.append("left")
+    return choice(possible_move_directions)
